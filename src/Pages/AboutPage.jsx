eb004c70c8c8b8e5f78f4d28ebdd60f2c6b7b464
Rohit Tiwari <110346246+tiwaritech18@users.noreply.github.com>
2023-04-10 21:54:43 +0530

    initial

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -5,40 +5,36 @@ import "./Pages.css";
 export default function AboutPage(){
   return (
     <div className="container">
-      <div className="about-us">
-        <div className="img">
-          <div className="img-bg"></div>
-           <img src="https://www.startingbusiness.com/blog/uploads/article/709/car-dealership.jpg" alt="car-buisness" />
+      <div className="about-us-section">
+        <div className="img-section">
+
+            <img src="https://hips.hearstapps.com/hmg-prod/images/car-dealer-showing-new-car-to-young-couple-in-royalty-free-image-1591622064.jpg?crop=0.66661xw:1xh;center,top&resize=1200:*" alt="" />
+     
+          <div className="img-bg">
+
+          </div>
         </div>
-       
         <div className="about-us-text">
-          <div className="about-us-heading">
-            <h2>About Us</h2>
-            <h4>Find out briefly informatively about us</h4>
+          <div className="about-us-intro">
+            <h1 className='heading'>About Us</h1>
+            <h3>Fine out briefly but informatively about us</h3>
           </div>
-          <div className="about-us-desc">
-            <p>
-            We are a team of dedicated professionals who are committed to making your car rental experience smooth and hassle-free. Our fleet of cars is well-maintained and regularly serviced to ensure that you have a safe and comfortable journey.
-            Whether you need a car for business or leisure, we have a wide range of vehicles to choose from. From compact cars to SUVs, we have something for everyone. Our rental rates are competitive, and we offer flexible rental options to meet your needs.
-            </p>
-          </div>
-          <div className="about-us-shortinfo">
-            <div className="time-management">
-              <AccessTimeIcon />
-              <p>
-                24/7 Exclusive On the road
-              </p>
-            </div>
-            <div className="luxury-desc">
-              <DirectionsCarIcon />
-              <p>From buisness to Luxuryclass</p>
-            </div>
-            <div className="online-book">
-              <InstallMobileIcon />
-              <p>Convinient Online Book</p>
-            </div>
-          </div>
+          <p>We are a team of dedicated professionals who are committed to making your car rental experience smooth and hassle-free. Our fleet of cars is well-maintained and regularly serviced to ensure that you have a safe and comfortable journey.Whether you need a car for business or leisure, we have a wide range of vehicles to choose from. From compact cars to SUVs, we have something for everyone. Our rental rates are competitive, and we offer flexible rental options to meet your needs.</p>
           
+          <div className="about-main-text-icon">
+            <div className="main-tools">
+              <AccessTimeIcon />
+              <p>24/7 Exclusive On the road</p>
+            </div>
+            <div className="main-tools">
+              <DirectionsCarIcon />
+              <p>From buisness to luxuryclass</p>
+            </div>
+            <div className="main-tools">
+              <InstallMobileIcon />
+              <p>Convenient online book</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/Pages/Catalogue.jsx
@@ -0,0 +1,7 @@
+export default function Catalogue(){
+  return (
+    <div>
+      
+    </div>
+  )
+}
